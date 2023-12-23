757c146b20af3ce3eb2ee9e03930e7665cc37d03
nirob_ahmed <122157741+nirobahmed0298@users.noreply.github.com>
2023-12-23 20:20:24 +0600

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,17 @@
+(function($){
+    'use strict';
+
+    // Mobile Menu //
+    $(".menuicon").on('click',function(){
+        $(".navlist").animate({left:0})
+    });
+    $(".x-mark i").on('click',function(){
+        $(".navlist").animate({left:-250})
+    });
+
+
+
+
+
+
+})(jQuery);
